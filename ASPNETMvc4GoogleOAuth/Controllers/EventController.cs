74819a69901eb733bb4a922b908a9071c9e3c89e
Kai Chu Chung <cage.chung@gmail.com>
2014-01-16 13:52:46 +0800

    add angularjs module

---
+++ ASPNETMvc4GoogleOAuth/Controllers/EventController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASPNETMvc4GoogleOAuth.Models;
+using ASPNETMvc4GoogleOAuth.Services;
+
+namespace ASPNETMvc4GoogleOAuth.Controllers
+{
+    public class EventController : Controller
+    {
+        private IGEventService service;
+
+        public EventController()
+        {
+            service = new GEventService(this.ModelState);
+        }
+
+        //
+        // GET: /Event/
+
+        public ActionResult Index()
+        {
+            if (Session["googletoken"] == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            return View();
+        }
+
+
+        // AJAX
+        // POST:/Event/GetAll
+        [HttpPost]
+        public JsonResult GetAll()
+        {
+            var gevents = service.GetAll();
+
+            return Json(gevents);
+        }
+
+         // AJAX
+        // POST /Event/FetchGoogleCalendar
+        [HttpPost]
+        public JsonResult FetchGoogleCalendar()
+        {
+            string token = Session["googletoken"] as string;
+            var calendarList = service.GetAllGoogleCalendar(token);
+            return Json(calendarList);
+        }
+
+        // AJAX
+        // POST /Event/Create
+        [HttpPost]
+        public JsonResult Create(string calendarId, string requestBody)
+        {
+            string token = Session["googletoken"] as string;
+            var e = service.Insert(token, calendarId, requestBody);
+
+            return Json(e);
+        }
+
+        // AJAX
+        // POST:/Event/GetEvent
+        [HttpPost]
+        public ActionResult GetEventByID(string id)
+        {
+            Guid _id;
+            Guid.TryParse(id, out _id);
+            if (_id == default(Guid))
+                return Redirect("Index");
+
+            var devent = service.FindAll().Where(x => x.guid == _id).SingleOrDefault();
+
+            return Json(devent);
+        }
+
+        // AJAX
+        // POST:/Event/UpdateEventByID
+        [HttpPost]
+        public JsonResult UpdateEventByID(string guid, string calendarId, string id, string requestBody)
+        {
+            string token = Session["googletoken"] as string;
+            var updatedGvent = service.Update(token, guid, calendarId, id, requestBody);
+
+            return Json(updatedGvent);
+        }
+
+        // AJAX
+        // POST:/Event/DeleteEventByID
+        [HttpPost]
+        public JsonResult DeleteEventByID(string guid, string calendarId, string id)
+        {
+            string token = Session["googletoken"] as string;
+
+            var b = service.Delete(token, guid, calendarId, id);
+
+            return Json(b);
+        }
+    }
+}
---
+++ ASPNETMvc4GoogleOAuth/Scripts/js/google-calendar-event.js
@@ -0,0 +1,216 @@
+﻿angular.module('gevent', ['ngRoute', 'ngResource', 'gevent.service', 'gevent.controller'])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider
+        .when('/', {
+            templateUrl: '/Scripts/js/list.html',
+            controller: 'listCtrl',
+            resolve: {
+                _events: ['eventsLoader', function (eventsLoader) { return eventsLoader(); }]
+            }
+        })
+        .when('/add', {
+            templateUrl: '/Scripts/js/add.html',
+            controller: 'addCtrl',
+            resolve: {
+                googleCalendars: ['googleCalendarLoader', function (googleCalendarLoader) { return googleCalendarLoader(); }]
+            }
+        })
+        .when('/edit/:id', {
+            templateUrl: '/Scripts/js/edit.html',
+            controller: 'editCtrl',
+            resolve: {
+                _event: ['$route', 'eventLoader', function ($route, eventLoader) {                    
+                    return eventLoader($route.current.params.id);
+                }]
+            }
+        })
+        .otherwise({ redirectTo: '/' });
+    }]);
+
+// service 
+angular.module('gevent.service', [])
+    .factory('GEVENT', ['$resource', function ($resource) {
+        var resetUrl = '/Event';
+        var resource = $resource(resetUrl + '/:action',
+          {
+
+          }, {
+              getAll: {
+                  method: 'POST',
+                  params: {
+                      action: "GetAll"
+                  },
+                  isArray: true
+              },
+              fetchGoogleCalendar: {
+                  method: 'POST',
+                  params: {
+                      action: 'FetchGoogleCalendar'
+                  },
+                  isArray: true
+              },
+              create: {
+                  method: 'POST',
+                  params: {
+                      action: 'Create'
+                  }
+              },
+              getEventByID: {
+                  method: 'POST',
+                  params: {
+                      action: 'GetEventByID'
+                  }
+              },
+              updateEventByID: {
+                  method: 'POST',
+                  params: {
+                      action: 'UpdateEventByID'
+                  }
+              },
+              deleteEventByID: {
+                  method: 'POST',
+                  params: {
+                      action: 'DeleteEventByID'
+                  }
+              }
+          });
+
+        return resource;
+    }])
+    .factory('eventsLoader', ['$q', 'GEVENT', function ($q, GEVENT) {
+        return function () {
+            var delay = $q.defer();
+            GEVENT.getAll(function (data) {
+                delay.resolve(data);
+            }, function () {
+                delay.reject('Unable to fetch database gevent list');
+            });
+            return delay.promise;
+        };
+    }])
+    .factory('googleCalendarLoader', ['$q', 'GEVENT', function ($q, GEVENT) {
+        return function () {
+            var delay = $q.defer();
+            GEVENT.fetchGoogleCalendar(function (data) {
+                delay.resolve(data);
+            }, function () {
+                delay.reject('Unable to fetch google calendar list');
+            });
+            return delay.promise;
+        };
+    }])
+    .factory('eventLoader', ['$q', 'GEVENT', function ($q, GEVENT) {
+        return function (id) {
+            var delay = $q.defer();            
+            GEVENT.getEventByID({ 'id': id }, function (data) {
+                delay.resolve(data);
+            }, function () {
+                delay.reject('Unable to fetch database event by id');
+            });
+            return delay.promise;
+        };
+    }]);
+
+// controller
+angular.module('gevent.controller', [])
+    .controller('listCtrl', ['$scope', '_events', 'GEVENT', function ($scope, _events, GEVENT) {
+        $scope.events = _events;
+
+        $scope.delete = function (g) {
+            GEVENT.deleteEventByID({
+                guid: g.guid,
+                calendarId: g.calendarId,
+                id: g.Id
+            }, function (status) {
+                if (status) {
+                    GEVENT.getAll(function (data) {
+                        $scope.events = data;
+                    }, function () {
+                        
+                    });
+                }
+            }, function (error) {
+                console.error(error);
+            });
+        };
+    }])
+    .controller('addCtrl', ['$scope', '$location', 'googleCalendars', 'GEVENT', function ($scope, $location, googleCalendars, GEVENT) {
+        // init
+        $scope.template = '/Scripts/js/template/_edit.html?t=' + +new Date();
+        $scope.calendars = (function (calendar) {
+            angular.forEach(calendar, function (item, index) {
+                item.selected = false;
+            });
+            return calendar;
+        })(googleCalendars);
+
+        // event post request body
+        $scope.event = {
+            calendarId: '',
+            requestBody: {
+                start: {
+                    date: moment().format('YYYY-MM-DD')
+                },
+                end: {
+                    date: moment().add(3, 'd').format('YYYY-MM-DD')
+                }
+            }
+        };
+
+        $scope.setSelect = function (Id) {
+            angular.forEach($scope.calendars, function (item, index) {
+                if (item.Id == Id)
+                    item.selected = true;
+                else
+                    item.selected = false;
+            });
+            $scope.event.calendarId = Id;
+        };5
+
+        $scope.create = function (r) {           
+            r.requestBody = JSON.stringify(r.requestBody);
+      
+            GEVENT.create({
+                calendarId: r.calendarId,
+                requestBody: r.requestBody
+            }, function (data) {
+                $location.path('/');
+            }, function (error) {
+                console.log(error);
+            });
+        };
+    }])
+    .controller('editCtrl', ['$scope', '$location', '_event', 'GEVENT', function ($scope, $location, _event, GEVENT) {
+        $scope.template = '/Scripts/js/template/_edit.html?t='+ +new Date();
+
+        $scope.event = {
+            guid:_event.guid,
+            calendarId: _event.calendarId,
+            id:_event.Id,
+            requestBody: {
+                start: {
+                    date:moment(_event.start).format('YYYY-MM-DD')
+                },
+                end:{
+                    date: moment(_event.end).format('YYYY-MM-DD')
+                },
+                summary : _event.summary,
+                description: _event.description                
+            }
+        };
+
+        $scope.update = function (r) {
+            r.requestBody = JSON.stringify(r.requestBody);
+
+            GEVENT.updateEventByID({
+                calendarId: r.calendarId,
+                guid: r.guid,
+                id: r.id,
+                requestBody: r.requestBody
+            }, function (data) {
+                $location.path('/');
+            }, function (error) {
+                console.error(error);
+            });
+        };
+    }]);
